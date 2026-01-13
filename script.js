a28d869d75177db58bbed5f7f429aacf1b28e873
vim <vimeannouv@hotmail.com>
2026-01-13 21:53:00 +1100

    finished the "createNavigationLink" function, added notes to the script, redid style to scale well with the view screen

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("hello world!!!")
+function createNavigationLinks() {
+    const headings = document.querySelectorAll(".content h3")
+    const sidebar = document.getElementById("sidebar")
+    const sidebarList = document.createElement("ul")
+
+    sidebar.appendChild(sidebarList)
+    headings.forEach((h3, index) => {
+
+        if (!h3.id) {
+            h3.id = `heading-${index}` // `${"..."} ==> string template`
+        }
+
+        console.log(h3.textContent)
+        
+        const li = document.createElement("li")
+        const a = document.createElement("a")
+
+        a.href = `#${h3.id}` // hyperlink reference
+        a.textContent = h3.textContent
+
+        li.appendChild(a)
+        sidebarList.append(li)
+    })
+}
+
+function onLoaded() {
+    createNavigationLinks()
+}
+
+document.addEventListener("DOMContentLoaded", onLoaded) // fires when the HTML document has been completely loaded and parsed, so basically on start
+
+/*
+               ~~ notes ~~
+obj.appendChild(childElement) ==> similar to "child.Parent = Instace".  Sends child to bottom
+
+*/
