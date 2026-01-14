3f2f9197d82eadfb4a1884a2b4dec716a13e8dc3
vim <vimeannouv@hotmail.com>
2026-01-14 18:33:48 +1100

    Turned writeContent into an async function instead of returning a promise

--- script.js
+++ script.js
@@ -22,28 +22,24 @@ function createNavigationLinks() {
     })
 }
 
-function writeContent() {
+async function writeContent() { // --> Promise
     const content = document.getElementById("content");
+    const response = await fetch("content.txt"); // ==> promise<Response>
+    const text = await response.text();
+    const lines = text.split('\n');
 
-    return fetch("content.txt") // ==> promise<Response>
-    .then(response => response.text()) // response ==> {return response.text()}
-    .then(text => {
-        const lines = text.split('\n')
-        lines.forEach(line => {
-            let element = null;
-            if (line.startsWith("#"))
-                element = document.createElement("h3");
-            else
-                element = document.createElement("p");
+    lines.forEach(line => {
+        let element = null;
+        if (line.startsWith("#"))
+            element = document.createElement("h3");
 
-            element.textContent = line.replace(/^#+\s*/, '');
-            content.appendChild(element);
-        });
-    })
-    .catch(reason => 
-        {console.log(reason)}
-    );
-}
+        else
+            element = document.createElement("p");
+
+        element.textContent = line.replace(/^#+\s*/, '');
+        content.appendChild(element);
+    });
+} 
 
 function onLoaded() {
     writeContent()
