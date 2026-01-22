92f1fa53afdd80dd575c52e1ae6ad0b56f7a91fc
vim <vimeanouv@gmail.com>
2026-01-22 14:29:41 +1100

    removed auth key. added a functions folder for netlify

--- scripts/main.js
+++ scripts/main.js
@@ -89,14 +89,9 @@ function writeInCodeblock() {
         pre.appendChild(code)
         
         let gitApi = null;
-
         gitApi = "https://api.github.com/repos/vimeannouv/december_project_SOE/contents/index.html"
 
-        const repository = fetch(gitApi, {
-            headers: {
-                "Authorization": "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"
-            }
-        });
+        const repository = fetch(gitApi);
         repository.then(resp => resp.json())
         .then(json => json.content)
         .then(src => atob(src)) //atob decodes base64 strs
