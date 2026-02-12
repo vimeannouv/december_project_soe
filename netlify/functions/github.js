ef225d65b640d1d08f62f83df18cd1d98151f902
vimeannouv <vimeanouv@gmail.com>
2026-02-12 21:07:38 +1100

    fixed details having too large of a bottom margin

--- netlify/functions/github.js
+++ netlify/functions/github.js
@@ -1,4 +1,4 @@
-const files = ["index.html", "scripts/main.js"];
+const files = ["index.html", "scripts/main.js", "scripts/weather-api.js"];
 
 export async function handler() {
   const key = process.env.GITHUB_TOKEN;
